7cfffeca68a92f45a0ad7573d274b44e2db5882d
mrjsng <>
2022-04-14 08:52:43 +0000

    Refactor: move operators, classifier to builtin module

--- builtin.py
+++ builtin.py
@@ -20,3 +20,54 @@ OPERATORS = [
 ]
 
 SYMBOLS = [':']
+
+
+
+# Operators
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
+def lt(x, y):
+    return x < y
+
+def lte(x, y):
+    return x <= y
+
+def gt(x, y):
+    return x > y
+
+def gte(x, y):
+    return x >= y
+
+def ne(x, y):
+    return x != y
+
+def eq(x, y):
+    return x == y
+
+
+
+# Classifier
+
+operators = {
+    '+': add,
+    '-': sub,
+    '*': mul,
+    '/': div,
+    '<': lt,
+    '<=': lte,
+    '>': gt,
+    '>=': gte,
+    '<>': ne,
+    '=': eq,
+}
