acc19e752dafd7fead8a5a59b445e9f96c787a5d
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-04-15 07:20:22 +0000

    Add main program

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+
+from builtin import ParseError, RuntimeError
+import scanner
+import parser
+import interpreter
+
+
+
+src = 'OUTPUT "Hello", ", ", "everyone", "!"'
+
+
+
+def main():
+    try:
+        tokens = scanner.scan(src)
+        statements = parser.parse(tokens)
+    except ParseError as err:
+        print(err)
+        sys.exit(65)
+    try:
+        interpreter.interpret(statements)
+    except RuntimeError as err:
+        print(err)
+        sys.exit(70)
