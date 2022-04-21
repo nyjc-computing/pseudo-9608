1b9182790d1c8e9bc7dfa75c986fbbadcac2497f
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-04-21 09:35:49 +0000

    syntax(interpreter): Swap evaluate() param order for consistency

--- interpreter.py
+++ interpreter.py
@@ -2,7 +2,7 @@ from builtin import RuntimeError
 
 
 
-def evaluate(expr, frame=None):
+def evaluate(frame, expr):
     # Evaluating tokens
     if 'type' in expr:
         if expr['type'] == 'name':
@@ -13,36 +13,36 @@ def evaluate(expr, frame=None):
         return expr
     # Evaluating exprs
     oper = expr['oper']['value']
-    left = evaluate(expr['left'])
-    right = evaluate(expr['right'])
+    left = evaluate(frame, expr['left'])
+    right = evaluate(frame, expr['right'])
     return oper(left, right)
 
 def execOutput(frame, stmt):
     for expr in stmt['exprs']:
-        print(str(evaluate(expr, frame)), end='')
+        print(str(evaluate(frame, expr)), end='')
     print('')  # Add \n
 
 def execInput(frame, stmt):
-    name = evaluate(stmt['name'], frame)
+    name = evaluate(frame, stmt['name'])
     frame[name]['value'] = input()
 
 def execDeclare(frame, stmt):
     pass
 
 def execAssign(frame, stmt):
-    name = evaluate(stmt['name'], frame)
-    value = evaluate(stmt['expr'], frame)
+    name = evaluate(frame, stmt['name'])
+    value = evaluate(frame, stmt['expr'])
     frame[name]['value'] = value
 
 def execCase(frame, stmt):
-    cond = evaluate(stmt['cond'], frame)
+    cond = evaluate(frame, stmt['cond'])
     if cond in stmt['stmts']:
         execute(frame, stmt['stmts'][cond])
     elif stmt['fallback']:
         execute(frame, stmt['fallback'])
 
 def execIf(frame, stmt):
-    if evaluate(stmt['cond'], frame):
+    if evaluate(frame, stmt['cond']):
         for substmt in stmt['stmts'][True]:
             execute(frame, substmt)
     elif stmt['fallback']:
@@ -52,14 +52,14 @@ def execIf(frame, stmt):
 def execWhile(frame, stmt):
     if stmt['init']:
         execute(frame, stmt['init'])
-    while evaluate(stmt['cond'], frame) is True:
+    while evaluate(frame, stmt['cond']) is True:
         for loopstmt in stmt['stmts']:
             execute(frame, loopstmt)
 
 def execRepeat(frame, stmt):
     for loopstmt in stmt['stmts']:
         execute(frame, loopstmt)
-    while evaluate(stmt['cond'], frame) is False:
+    while evaluate(frame, stmt['cond']) is False:
         for loopstmt in stmt['stmts']:
             execute(frame, loopstmt)
 
@@ -68,11 +68,11 @@ def execProcedure(frame, stmt):
 
 def execCall(frame, stmt):
     # Get procedure from frame
-    proc = evaluate(stmt['name'], frame)
+    proc = evaluate(frame, stmt['name'])
     # Assign args into frame with param names
     args, params = stmt['args'], proc['params']
     for arg, var, param in zip(args, params.keys(), params.values()):
-        frame[var]['value'] = evaluate(arg, frame)
+        frame[var]['value'] = evaluate(frame, arg)
     for callstmt in proc['stmts']:
         execute(frame, callstmt)
 
