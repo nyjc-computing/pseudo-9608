0ab95e42597a6ede5e6d88de4ef39be5f8885725
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-04-27 07:43:24 +0000

    syntax(lang): Add Expr superclass and subclasses

--- lang.py
+++ lang.py
@@ -1,2 +1,35 @@
 class Expr:
-    pass
+    def resolve(self):
+        raise NotImplementedError
+
+    def evaluate(self):
+        raise NotImplementedError
+
+
+
+class Unary(Expr):
+    def __init__(self, oper, right):
+        self.oper = oper
+        self.right = right
+
+
+
+class Binary(Expr):
+    def __init__(self, left, oper, right):
+        self.left = left
+        self.oper = oper
+        self.right = right
+
+
+
+class Get(Expr):
+    def __init__(self, frame, name):
+        self.frame = frame
+        self.name = name
+
+
+
+class Call(Expr):
+    def __init__(self, callable, args):
+        self.callable = callable
+        self.args = args
