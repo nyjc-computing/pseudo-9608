4af9a6f9488635ab967f8a4e0701ea8146a5a794
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-04-28 04:07:17 +0000

    fix(main): Handle missing token

--- main.py
+++ main.py
@@ -22,8 +22,9 @@ def main():
         statements = parser.parse(tokens)
         statements, frame = resolver.inspect(statements)
     except (ParseError, LogicError) as err:
-        lineinfo = f"[Line {err.line}]" if err.line else ""
-        print(lineinfo, lines[err.line - 1])
+        if err.line:
+            lineinfo = f"[Line {err.line}]"
+            print(lineinfo, lines[err.line - 1])
         if err.col:
             leftmargin = len(lineinfo) + 1 + err.col
             print((' ' * leftmargin) + '^')
