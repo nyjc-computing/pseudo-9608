90b797c089119694a87600485498ce7c3a58eaec
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-05-05 02:38:17 +0000

    feat(resolver): Handle boolean comparisons

--- resolver.py
+++ resolver.py
@@ -54,7 +54,15 @@ def resolveUnary(frame, expr):
 def resolveBinary(frame, expr):
     lefttype = expr.left.accept(frame, resolve)
     righttype = expr.right.accept(frame, resolve)
-    if expr.oper in (gt, gte, lt, lte, ne, eq):
+    if expr.oper in (ne, eq):
+        if lefttype not in ('BOOLEAN', 'INTEGER'):
+            raise LogicError(f"Invalid comparison type", token=expr.left.token())
+        if righttype not in ('BOOLEAN', 'INTEGER'):
+            raise LogicError(f"Invalid comparison type", token=expr.right.token())
+        if lefttype != righttype:
+            raise LogicError(f"Illegal comparison between {lefttype} and {righttype}", token=expr.oper.token())
+        return 'BOOLEAN'
+    if expr.oper in (gt, gte, lt, lte):
         expectTypeElseError(lefttype, 'INTEGER', token=expr.left.token())
         expectTypeElseError(righttype, 'INTEGER', token=expr.right.token())
         return 'BOOLEAN'
