55c1d4f50b3cb7ace53cb8ac310bf650f9be9b1f
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-05-06 06:40:08 +0000

    test(pseudocode): Add tests for PROCEDURE

--- tests/test_procedure.py
+++ tests/test_procedure.py
@@ -11,5 +11,20 @@ class ProcedureTestCase(unittest.TestCase):
         self.result = pseudocode.run(src)
         
     def test_procedure(self):
-        pass
-        
+        frame = self.result['frame']
+        self.assertIs(
+            frame.getType('TestBool'),
+            'NULL',
+        )
+        self.assertIs(
+            type(frame.getValue('TestBool')),
+            pseudocode.lang.Procedure,
+        )
+        procedure = frame.getValue('TestBool')
+        self.assertTrue(
+            procedure.frame.getValue('Succeeded')
+        )
+        self.assertEqual(
+            procedure.frame.getType('Succeeded'),
+            'BOOLEAN'
+        )
