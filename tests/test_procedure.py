236ca069ae3480a263a92c20d3c141d69018f751
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-05-06 05:51:38 +0000

    test(pseudocode): Run interpreter in test case

--- tests/test_procedure.py
+++ tests/test_procedure.py
@@ -1,11 +1,14 @@
 import unittest
 
+import pseudocode
+
 file = 'procedure.pseudo'
 
 class ProcedureTestCase(unittest.TestCase):
     def setUp(self):
         with open(file) as f:
             src = f.read()
+        pseudocode.run(src)
         # Pass src to interpreter
         # Retrieve result: frame, errors, output, etc
         
