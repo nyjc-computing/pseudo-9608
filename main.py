9b765d9c2cac855b303cd2b04d91c4b4d4313df0
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-05-06 00:30:00 +0000

    pkg(pseudo): Migrate main program to __init__

--- main.py
+++ main.py
@@ -1,44 +1,6 @@
-import sys
-from pprint import PrettyPrinter
-
-from pseudo.builtin import ParseError, RuntimeError, LogicError
-from pseudo import scanner
-from pseudo import parser
-from pseudo import resolver
-from pseudo import interpreter
-
-
-
-def main():
-    if len(sys.argv) == 1:
-        srcfile = 'main.pseudo'
-    else:
-        srcfile = sys.argv[1]
-    with open(srcfile, 'r') as f:
-        src = f.read()
-    pp = PrettyPrinter(indent=2, compact=True)
-    try:
-        tokens, lines = scanner.scan(src)
-        statements = parser.parse(tokens)
-        statements, frame = resolver.inspect(statements)
-    except (ParseError, LogicError) as err:
-        errType = type(err).__name__ + ':'
-        if err.line:
-            lineinfo = f"[Line {err.line}]"
-            print(lineinfo, lines[err.line - 1])
-        if err.col:
-            leftmargin = len(lineinfo) + 1 + err.col
-            print((' ' * leftmargin) + '^')
-        print(errType, err.report())
-        sys.exit(65)
-    try:
-        frame = interpreter.interpret(statements, frame)
-        pp.pprint(frame)
-    except RuntimeError as err:
-        print(err)
-        sys.exit(70)
+import pseudo
 
 
 
 if __name__ == "__main__":
-    main()
+    pseudo.run()
