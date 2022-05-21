a77f14baf1a29eb38cd3cbca6f5d07118393573f
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-05-21 10:09:32 +0000

    doc(lang): Add type annotation

--- pseudocode/lang.py
+++ pseudocode/lang.py
@@ -1,111 +1,39 @@
+from typing import Any, Union, Iterable, Mapping
+from typing import Callable as function, TextIO
+
+# Pseudocode types
+Type = str  # Built-in and declared types
+Name = str  # Variable names
+Index = tuple  # Array indexes
+Key = Union[Name, Index]  # in TypedValue
+Lit = Union[bool, int, float, str]  # Simple data types
+Val = Union[Lit, "Value"]  # in TypedValue
+Param = Union["Get", "TypedValue"]  # Callable params
+Arg = "Expr"  # Call args
+Rule = str  # Stmt rules
+
+# ----------------------------------------------------------------------
 class Token:
     """
     Encapsulates data for a token.
     """
-    def __init__(self, line, column, type, word, value):
+    def __init__(
+        self,
+        line: int,
+        column: int,
+        type: Type,
+        word: str,
+        value: Any,
+    ) -> None:
         self.line = line
         self.col = column
         self.type = type
         self.word = word
         self.value = value
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         lineinfo = f"[Line {self.line} column {self.col}]"
         return f"{lineinfo} <{self.value}> {repr(self.word)}"
-
-
-
-class Value:
-    """
-    Base class for pseudo values.
-    Represents a value stored in the frame.
-    """
-
-
-
-class Callable(Value):
-    """
-    Base class for Function and Procedure.
-    Represents a Callable in pseudo.
-
-    Attributes
-    ----------
-    - frame
-        The frame used by the callable
-    - params
-        A list of parameters used by the callable
-    - stmts
-        A list of statements the callable executes when called
-    """
-    __slots__ = ('frame', 'params', 'stmts')
-    def __init__(self, frame, params, stmts):
-        self.frame = frame
-        self.params = params
-        self.stmts = stmts
-
-    def __repr__(self):
-        attrstr = ", ".join([
-            repr(getattr(self, attr)) for attr in self.__slots__
-        ])
-        return f'{type(self).__name__}({attrstr})'
-
-
-
-class Function(Callable):
-    """Functions are evaluated to return a value."""
-
-
-
-class Procedure(Callable):
-    """Procedures are called to execute its statements."""
-
-
-
-class Builtin(Value):
-    """
-    Represents a system function in pseudo.
-
-    Attributes
-    ----------
-    - params
-        A list of parameters used by the callable
-    - func
-        the Python function to call when invoked
-    """
-    __slots__ = ('params', 'func')
-    def __init__(self, params, func):
-        self.params = params
-        self.func = func
-
-    def __repr__(self):
-        attrstr = ", ".join([
-            repr(getattr(self, attr)) for attr in self.__slots__
-        ])
-        return f'{type(self).__name__}({attrstr})'
-
-
-
-class File(Value):
-    """
-    Represents a file object in a frame.
-
-    Attributes
-    ----------
-    - name
-        Name of the file that is open
-    - mode
-        The mode that the file was opened in
-    - iohandler
-        An object for accessing the file
-    """
-    __slots__ = ('name', 'mode', 'iohandler')
-    def __init__(self, name, mode, iohandler):
-        self.name = name
-        self.mode = mode
-        self.iohandler = iohandler
-
-    def __repr__(self):
-        return f"<{self.mode}: {self.name}>"
 
 
 
@@ -127,29 +55,32 @@ class TypeSystem:
     getTemplate(name)
         return the template of the type
     """
-    def __init__(self, *types):
+    def __init__(
+        self,
+        *types: Type,
+    ) -> None:
         self.data = {}
         for typeName in types:
             self.declare(typeName)
             self.setTemplate(typeName, None)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         nameTypePairs = [
             f"{name}: {self.getTemplate(name)}"
             for name in self.data
         ]
         return f"{{{', '.join(nameTypePairs)}}}"
 
-    def has(self, name):
+    def has(self, name: str) -> bool:
         return name in self.data
 
-    def declare(self, name):
+    def declare(self, name: str) -> None:
         self.data[name] = None
 
-    def setTemplate(self, name, template):
+    def setTemplate(self, name: str, template: "Object") -> None:
         self.data[name] = TypedValue(name, template)
 
-    def getTemplate(self, name):
+    def getTemplate(self, name) -> "Object":
         return self.data[name]
 
 
@@ -160,15 +91,21 @@ class TypedValue:
     Each TypedValue has a type and a value.
     """
     __slots__ = ('type', 'value')
-    def __init__(self, type, value, *args, **kwargs):
+    def __init__(
+        self,
+        type: Type,
+        value: Val,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
         super().__init__(*args, **kwargs)
         self.type = type
         self.value = value
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<{self.type}: {repr(self.value)}>"
 
-    def copy(self):
+    def copy(self) -> "TypedValue":
         """This returns an empty copy of the typedvalue"""
         Class = type(self)
         if isinstance(self.value, Object):
@@ -177,7 +114,15 @@ class TypedValue:
 
 
 
-class Object:
+class Value:
+    """
+    Base class for pseudo values.
+    Represents a value stored in the frame.
+    """
+
+
+
+class Object(Value):
     """
     Represents a space for storing of TypedValues in 9608 pseudocode.
     Provides methods for managing TypedValues.
@@ -201,37 +146,40 @@ class Object:
     copy()
         return a copy of the object
     """
-    def __init__(self, typesys):
+    def __init__(
+        self,
+        typesys: "TypeSystem",
+    ) -> None:
         self.data = {}
         self.types = typesys
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         nameTypePairs = [
             f"{name}: {self.getType(name)}"
             for name in self.data
         ]
         return f"{{{', '.join(nameTypePairs)}}}"
 
-    def has(self, name):
+    def has(self, name: Key) -> bool:
         return name in self.data
 
-    def declare(self, name, type):
+    def declare(self, name: Key, type: str) -> None:
         template = self.types.getTemplate(type)
         self.data[name] = template.copy()
 
-    def getType(self, name):
+    def getType(self, name: Key) -> Type:
         return self.data[name].type
 
-    def getValue(self, name):
+    def getValue(self, name: Key) -> Val:
         return self.data[name].value
 
-    def get(self, name):
+    def get(self, name: Key) -> "TypedValue":
         return self.data[name]
 
-    def setValue(self, name, value):
+    def setValue(self, name: Key, value: Any) -> None:
         self.data[name].value = value
 
-    def copy(self):
+    def copy(self) -> "Object":
         """This returns an empty copy of the object"""
         Class = type(self)
         newobj = Class(typesys=self.types)
@@ -256,17 +204,21 @@ class Frame(Object):
     lookup(name)
         returns the first frame containing the name
     """
-    def __init__(self, typesys, outer=None):
+    def __init__(
+        self,
+        typesys: "TypeSystem",
+        outer: "Frame"=None,
+    ) -> None:
         super().__init__(typesys=typesys)
         self.outer = outer
 
-    def set(self, name, typedValue):
+    def set(self, name, typedValue) -> None:
         self.data[name] = typedValue
 
-    def delete(self, name):
+    def delete(self, name) -> None:
         del self.data[name]
 
-    def lookup(self, name):
+    def lookup(self, name) -> "Frame":
         if self.has(name):
             return self
         if self.outer:
@@ -286,9 +238,109 @@ class Array(Object):
        The common type of each element
     """
     @property
-    def elementType(self):
+    def elementType(self) -> Type:
         for elem in self.data.values():
             return elem.type
+
+
+
+class Builtin(Value):
+    """
+    Represents a system function in pseudo.
+
+    Attributes
+    ----------
+    - params
+        A list of parameters used by the callable
+    - func
+        the Python function to call when invoked
+    """
+    __slots__ = ('params', 'func')
+    def __init__(
+        self,
+        params: Iterable,
+        func: function,
+    ) -> None:
+        self.params = params
+        self.func = func
+
+    def __repr__(self) -> str:
+        attrstr = ", ".join([
+            repr(getattr(self, attr)) for attr in self.__slots__
+        ])
+        return f'{type(self).__name__}({attrstr})'
+
+
+
+class Callable(Value):
+    """
+    Base class for Function and Procedure.
+    Represents a Callable in pseudo.
+
+    Attributes
+    ----------
+    - frame
+        The frame used by the callable
+    - params
+        A list of parameters used by the callable
+    - stmts
+        A list of statements the callable executes when called
+    """
+    __slots__ = ('frame', 'params', 'stmts')
+    def __init__(
+        self,
+        frame: "Frame",
+        params: Iterable[Param],
+        stmts: Iterable["Stmt"],
+    ) -> None:
+        self.frame = frame
+        self.params = params
+        self.stmts = stmts
+
+    def __repr__(self) -> str:
+        attrstr = ", ".join([
+            repr(getattr(self, attr)) for attr in self.__slots__
+        ])
+        return f'{type(self).__name__}({attrstr})'
+
+
+
+class Function(Callable):
+    """Functions are evaluated to return a value."""
+
+
+
+class Procedure(Callable):
+    """Procedures are called to execute its statements."""
+
+
+
+class File(Value):
+    """
+    Represents a file object in a frame.
+
+    Attributes
+    ----------
+    - name
+        Name of the file that is open
+    - mode
+        The mode that the file was opened in
+    - iohandler
+        An object for accessing the file
+    """
+    __slots__ = ('name', 'mode', 'iohandler')
+    def __init__(
+        self,
+        name: Name,
+        mode: str,
+        iohandler: TextIO,
+    ) -> None:
+        self.name = name
+        self.mode = mode
+        self.iohandler = iohandler
+
+    def __repr__(self) -> str:
+        return f"<{self.mode}: {self.name}>"
 
 
 
@@ -304,17 +356,20 @@ class Expr:
         Returns the token asociated with the expr, for error
         reporting purposes.
     """
-    def __init__(self, token=None):
+    def __init__(
+        self,
+        token: "Token"=None,
+    ) -> None:
         self._token = token
 
-    def token(self):
-        return self._token
-
-    def __repr__(self):
+    def __repr__(self) -> str:
         attrstr = ", ".join([
             repr(getattr(self, attr)) for attr in self.__slots__
         ])
         return f'{type(self).__name__}({attrstr})'
+
+    def token(self) -> "Token":
+        return self._token
 
 
 
@@ -328,7 +383,11 @@ class Literal(TypedValue, Expr):
 
 class Name(Expr):
     __slots__ = ('name',)
-    def __init__(self, name, token=None):
+    def __init__(
+        self,
+        name: Name,
+        token: "Token"=None,
+    ) -> None:
         super().__init__(token=token)
         self.name = name
 
@@ -336,7 +395,13 @@ class Name(Expr):
 
 class Declare(Expr):
     __slots__ = ('name', 'type', 'metadata')
-    def __init__(self, name, type, metadata=None, token=None):
+    def __init__(
+        self,
+        name: Name,
+        type: Type,
+        metadata: Mapping=None,
+        token: "Token"=None,
+    ) -> None:
         super().__init__(token=token)
         self.name = name
         self.type = type
@@ -346,7 +411,12 @@ class Declare(Expr):
 
 class Assign(Expr):
     __slots__ = ('name', 'assignee', 'expr')
-    def __init__(self, name, assignee, expr, token=None):
+    def __init__(
+        self,
+        name: Name,
+        assignee: "Get",
+        expr: "Expr",
+        token: "Token"=None) -> None:
         super().__init__(token=token)
         self.name = name
         self.assignee = assignee
@@ -356,7 +426,12 @@ class Assign(Expr):
 
 class Unary(Expr):
     __slots__ = ('oper', 'right')
-    def __init__(self, oper, right, token=None):
+    def __init__(
+        self,
+        oper: function,
+        right: "Expr",
+        token: "Token"=None,
+    ) -> None:
         super().__init__(token=token)
         self.oper = oper
         self.right = right
@@ -365,7 +440,13 @@ class Unary(Expr):
 
 class Binary(Expr):
     __slots__ = ('left', 'oper', 'right')
-    def __init__(self, left, oper, right, token=None):
+    def __init__(
+        self,
+        left: "Expr",
+        oper: function,
+        right: "Expr",
+        token: "Token"=None,
+    ) -> None:
         super().__init__(token=token)
         self.left = left
         self.oper = oper
@@ -375,7 +456,12 @@ class Binary(Expr):
 
 class Get(Expr):
     __slots__ = ('frame', 'name')
-    def __init__(self, frame, name, token=None):
+    def __init__(
+        self,
+        frame: "Frame",
+        name: Name,
+        token: "Token"=None,
+    ) -> None:
         super().__init__(token=token)
         self.frame = frame
         self.name = name
@@ -384,7 +470,12 @@ class Get(Expr):
 
 class Call(Expr):
     __slots__ = ('callable', 'args')
-    def __init__(self, callable, args, token=None):
+    def __init__(
+        self,
+        callable: "Callable",
+        args: Iterable[Arg],
+        token: "Token"=None,
+    ) -> None:
         super().__init__(token=token)
         self.callable = callable
         self.args = args
@@ -392,7 +483,7 @@ class Call(Expr):
 
 
 class Stmt:
-    def __repr__(self):
+    def __repr__(self) -> str:
         attrstr = ", ".join([
             repr(getattr(self, attr)) for attr in self.__slots__
         ])
@@ -402,7 +493,11 @@ class Stmt:
 
 class ExprStmt(Stmt):
     __slots__ = ('rule', 'expr')
-    def __init__(self, rule, expr):
+    def __init__(
+        self,
+        rule: Rule,
+        expr: "Expr",
+    ) -> None:
         self.rule = rule
         self.expr = expr
 
@@ -410,7 +505,11 @@ class ExprStmt(Stmt):
 
 class Output(Stmt):
     __slots__ = ('rule', 'exprs')
-    def __init__(self, rule, exprs):
+    def __init__(
+        self,
+        rule: Rule,
+        exprs: Iterable["Expr"],
+    ) -> None:
         self.rule = rule
         self.exprs = exprs
 
@@ -418,7 +517,11 @@ class Output(Stmt):
 
 class Input(Stmt):
     __slots__ = ('rule', 'name')
-    def __init__(self, rule, name):
+    def __init__(
+        self,
+        rule: Rule,
+        name: Name,
+    ) -> None:
         self.rule = rule
         self.name = name
 
@@ -426,7 +529,13 @@ class Input(Stmt):
 
 class Conditional(Stmt):
     __slots__ = ('rule', 'cond', 'stmtMap', 'fallback')
-    def __init__(self, rule, cond, stmtMap, fallback):
+    def __init__(
+        self,
+        rule: Rule,
+        cond: "Expr",
+        stmtMap: Mapping[Lit, "Stmt"],
+        fallback: "Stmt",
+    ) -> None:
         self.rule = rule
         self.cond = cond
         self.stmtMap = stmtMap
@@ -436,7 +545,13 @@ class Conditional(Stmt):
 
 class Loop(Stmt):
     __slots__ = ('rule', 'init', 'cond', 'stmts')
-    def __init__(self, rule, init, cond, stmts):
+    def __init__(
+        self,
+        rule: Rule,
+        init: "Stmt",
+        cond: "Expr",
+        stmts: Iterable["Stmt"],
+    ) -> None:
         self.rule = rule
         self.init = init
         self.cond = cond
@@ -446,7 +561,15 @@ class Loop(Stmt):
 
 class ProcFunc(Stmt):
     __slots__ = ('rule', 'name', 'passby', 'params', 'stmts', 'returnType')
-    def __init__(self, rule, name, passby, params, stmts, returnType):
+    def __init__(
+        self,
+        rule: Rule,
+        name: Name,
+        passby: str,
+        params: Iterable[Param],
+        stmts: Iterable["Stmt"],
+        returnType: Type,
+    ) -> None:
         self.rule = rule
         self.name = name
         self.passby = passby
@@ -458,7 +581,12 @@ class ProcFunc(Stmt):
 
 class TypeStmt(Stmt):
     __slots__ = ('rule', 'name', 'exprs')
-    def __init__(self, rule, name, exprs):
+    def __init__(
+        self,
+        rule: Rule,
+        name: Name,
+        exprs: Iterable["Expr"],
+    ) -> None:
         self.rule = rule
         self.name = name
         self.exprs = exprs
@@ -467,7 +595,14 @@ class TypeStmt(Stmt):
 
 class FileAction(Stmt):
     __slots__ = ('rule', 'action', 'name', 'mode', 'data')
-    def __init__(self, rule, action, name, mode, data):
+    def __init__(
+        self,
+        rule: Rule,
+        action: str,
+        name: Name,
+        mode: str,
+        data: "Expr",
+    ) -> None:
         self.rule = rule
         self.action = action
         self.name = name
