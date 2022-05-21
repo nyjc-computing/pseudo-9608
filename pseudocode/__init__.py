51370ba010e2c8eddcf566bff5671f804ec8094d
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-05-21 10:22:12 +0000

    doc(pseudocode): Add type annotation

--- pseudocode/__init__.py
+++ pseudocode/__init__.py
@@ -1,4 +1,6 @@
-from .builtin import ParseError, RuntimeError, LogicError
+from typing import Iterable, Mapping, Callable as function
+
+import builtin
 from .lang import Frame
 from .system import system
 
@@ -12,7 +14,7 @@ __version__ = '0.2.1'
 
 
 
-def error(lines, err):
+def error(lines: Iterable, err: builtin.PseudoError) -> None:
     errType = type(err).__name__ + ':'
     if err.line:
         lineinfo = f"[Line {err.line}]"
@@ -26,24 +28,24 @@ def error(lines, err):
 
 class Pseudo:
     """A 9608 pseudocode interpreter"""
-    def __init__(self):
+    def __init__(self) -> None:
         self.handlers = {
             'output': print,
             'input': input,
         }
 
-    def registerHandlers(self, **kwargs):
+    def registerHandlers(self, **kwargs: Mapping[str, function]) -> None:
         for key, handler in kwargs.items():
             if key not in self.handlers:
                 raise KeyError(f"Invalid handler key {repr(key)}")
             self.handlers[key] = handler
 
-    def runFile(self, srcfile):
+    def runFile(self, srcfile: str) -> "Frame":
         with open(srcfile, 'r') as f:
             src = f.read()
         return self.run(src)
     
-    def run(self, src):
+    def run(self, src: str) -> "Frame":
         globalFrame = Frame(typesys=system.types, outer=system)
         result = {
             'lines': None,
@@ -56,7 +58,7 @@ class Pseudo:
             tokens, lines = scanner.scan(src)
             result['lines'] = lines
             statements = parser.parse(tokens)
-        except ParseError as err:
+        except builtin.ParseError as err:
             result['error'] = err
             return result
         except Exception as err:
@@ -66,7 +68,7 @@ class Pseudo:
         resolver = Resolver(globalFrame, statements)
         try:
             resolver.inspect()
-        except LogicError as err:
+        except builtin.LogicError as err:
             result['error'] = err
             return result
         except Exception as err:
@@ -77,7 +79,7 @@ class Pseudo:
         interpreter.registerOutputHandler(self.handlers['output'])
         try:
             interpreter.interpret()
-        except RuntimeError as err:
+        except builtin.RuntimeError as err:
             result['error'] = err
         except Exception as err:
             breakpoint()
