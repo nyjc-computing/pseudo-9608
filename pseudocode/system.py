5a49ee7e96029fcc4c03759863354c97539afd08
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-10-18 01:00:19 +0000

    feat(system): Added system functions
    These functions are mentioned in the 9608 appendix.

--- pseudocode/system.py
+++ pseudocode/system.py
@@ -12,6 +12,7 @@ from typing import (
     List,
     TextIO,
     Tuple,
+    Union,
 )
 
 from . import(builtin, lang)
@@ -38,6 +39,68 @@ def EOF(file: TextIO) -> bool:
     file.seek(pos)
     return iseof
 
+def LENGTH(thisStr: str) -> int:
+    """Returns integer value representing the length of thisStr."""
+    return len(str)
+
+def LEFT(thisStr: str, x: int) -> str:
+    """Returns leftmost x characters from thisStr."""
+    if x < 0:
+        raise builtin.RuntimeError("Expected integer >= 0")
+    if x > len(str):
+        raise builtin.RuntimeError("String length exceeded")
+    return thisStr[:x]
+
+def RIGHT(thisStr: str, x: int) -> str:
+    """Returns rightmost x characters from thisStr."""
+    if x < 0:
+        raise builtin.RuntimeError("Expected integer >= 0")
+    if x > len(str):
+        raise builtin.RuntimeError("String length exceeded")
+    return thisStr[-x:]
+
+def INT(x: float) -> int:
+    """Returns the integer part of x."""
+    return int(x)
+
+def MOD(thisNum: int, thisDiv: int) -> int:
+    """
+    Returns the integer value representing the remainder when
+    thisNum is divided by thisDiv.
+    """
+    return thisNum % thisDiv
+
+def MID(thisStr: str, x: int, y: int) -> str:
+    """
+    Returns string of length y starting at position x from thisStr.
+    """
+    pass
+
+def LCASE(thisChar: str) -> str:
+    """
+    Returns the character value representing the lower case equivalent
+    of thisChar.
+    If thisChar is not an uppercase alphabetic character, it is
+    returned unchanged.
+    """
+    # TODO: Change type signature to take CHAR type only
+    pass
+
+def DIV(thisNum: int, thisDiv: int) -> int:
+    """
+    Returns the integer value representing the whole number part of
+    the result when thisNum is divided by thisDiv.
+    """
+    pass
+
+def INT_TO_STRING(x: int) -> str:
+    """Returns a string representation of an INTEGER value."""
+    return str(x)
+
+def REAL_TO_STRING(x: float) -> str:
+    """Returns a string representation of a REAL value."""
+    return str(x)
+
 
 
 funcReturnParams: List[Tuple[function, lang.Type, List[lang.TypedValue]]] = [
@@ -47,6 +110,31 @@ funcReturnParams: List[Tuple[function, lang.Type, List[lang.TypedValue]]] = [
         lang.TypedValue(type='INTEGER', value=None),
     ]),
     (EOF, 'BOOLEAN', [lang.TypedValue(type='STRING', value=None)]),
+    (LENGTH, 'INTEGER', [lang.TypedValue(type='STRING', value=None)]),
+    (LEFT, 'STRING', [
+        lang.TypedValue(type='STRING', value=None),
+        lang.TypedValue(type='INTEGER', value=None),
+    ]),
+    (RIGHT, 'STRING', [
+        lang.TypedValue(type='STRING', value=None),
+        lang.TypedValue(type='INTEGER', value=None),
+    ]),
+    (INT, 'INTEGER', [lang.TypedValue(type='REAL', value=None)]),
+    (MOD, 'INTEGER', [
+        lang.TypedValue(type='INTEGER', value=None),
+        lang.TypedValue(type='INTEGER', value=None),
+    ]),
+    (MID, 'STRING', [
+        lang.TypedValue(type='INTEGER', value=None),
+        lang.TypedValue(type='INTEGER', value=None),
+    ]),
+    (LCASE, 'STRING', [lang.TypedValue(type='STRING', value=None)]),
+    (DIV, 'INTEGER', [
+        lang.TypedValue(type='INTEGER', value=None),
+        lang.TypedValue(type='INTEGER', value=None),
+    ]),
+    (INT_TO_STRING, 'STRING', [lang.TypedValue(type='INTEGER', value=None)]),
+    (REAL_TO_STRING, 'STRING', [lang.TypedValue(type='REAL', value=None)]),
 ]
 
 def initFrame() -> lang.Frame:
